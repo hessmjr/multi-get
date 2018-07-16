29e34c1c871aa3fe13500cf5a90e84ee2fdefbda
Mark <mark.hess2@gmail.com>
2018-07-15 23:48:14 -0500

    couple passing tests
    these should pass and prove Travis is working

--- tests/test.py
+++ tests/test.py
@@ -3,20 +3,26 @@ from unittest import TestCase, main
 import click
 from click.testing import CliRunner
 
-from __main__ import cli
+from multiget.__main__ import cli
+from multiget.download import Request, parse_filename
 
 
 class TestCli(TestCase):
 
-    def test_hello(self):
+    def test_good_url(self):
          runner = CliRunner()
-         result = runner.invoke(cli, ['Peter'])
+         result = runner.invoke(cli, ['https://httpbin.org/image/jpeg'])
          assert result.exit_code == 0
-         assert result.output == 'Hello Peter!\n'
+         assert result.output == 'File download complete.\n'
 
 
 class TestDownload(TestCase):
-    pass
+
+    def test_filename_parse(self):
+        url = 'https://httpbin.org/image/jpeg'
+        filename = 'jpeg'
+
+        assert filename == parse_filename(url)
 
 
 if __name__ == '__main__':
